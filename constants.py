0e1ab2a6dd622b24f0958d816a1991a0ce7decc0
小康2022 <392126563@qq.com>
2023-03-16 14:46:52 +0800

    version 0.0.2

--- constants.py
+++ constants.py
@@ -1,5 +1,30 @@
 """ 服务器常量 """
 
-PORT = 10086
+# 服务器
 
+NAME = 'Questioner'  # 服务器名称
+PORT = 32768  # 服务器端口
+MAXIMUM = 128  # 监听最大数量
+PACK_SIZE = 4096  # 单个包大小
 
+# 日志
+
+INFO = '\033[32mINFO\033[0m'  # 信息标识
+WARN = '\033[33mWARN\033[0m'  # 警告标识
+ERROR = '\033[31mERROR\033[0m'  # 错误标识
+
+# 邮件
+
+HOST = 'smtp.qq.com'  # QQ邮箱服务器地址
+USER = '2951256653'  # 用户名
+SENDER = '2951256653@qq.com'  # 邮件发送方邮箱地址
+TITLE = 'Questioner服务器验证码'  # 标题
+FROM = 'Questioner服务器'  # 发件人
+CODES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'  # 验证码可能值
+MSG = """
+<div style="font-family:楷体 ; background-color: lightyellow; border:3px solid black ;padding:10px">
+    <h2 style="text-align: center;">Questioner服务器验证码</h2>
+    <p style="text-align: center;">下面是您本次的验证码，10分钟内有效</p>
+    <h1 style="text-align: center;">%s</h1>
+    <p style="text-align: center;">若您未请求过验证码，那或许是他人误填了您的邮箱，请您忽略这封邮件</p>
+</div>"""  # 信息基本格式
--- server.py
+++ server.py
@@ -1,31 +1,66 @@
 """ 服务器 """
+
 import socket
 import threading
+import time
 
+import mail
 from constants import *
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = '小康2022<2951256653@qq.com>'
+
+logfile = open('Logs.log', 'a', encoding='utf-8')
+
+
+def output(mode: str, msg: str) -> None:
+    """ 终端信息输出 """
+    _time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    print('\r\033[36m%s\033[0m [%s] \033[30m>\033[0m %s' % (_time, mode, msg))
+    print('%s [%s] > %s' % (_time, mode[5:-4], msg), file=logfile, flush=True)
+
+
+output(INFO, '%s Server: v%s' % (NAME, __version__))
 
 
 class Server:
     """ 服务器 """
 
+    output(INFO, '创建服务端套接字')
     server = socket.socket()
+    output(INFO, '绑定端口: %s' % PORT)
     server.bind(('', PORT))
-    server.listen(128)
+    output(INFO, '设置服务端监听最大数量: %d' % MAXIMUM)
+    server.listen(MAXIMUM)
 
     @classmethod
     def accept(cls) -> None:
         """ 等待 """
-        print('SERVER LAUNCH')
         while True:
             try:
                 client, retaddress = cls.server.accept()
                 threading.Thread(
                     target=Client, args=(client, retaddress), daemon=True).start()
             except Exception as error:
-                print(error.__repr__())
+                output(ERROR, error.__repr__())
+
+    @classmethod
+    def input(cls, count: int = 0) -> None:
+        """ 终端命令输入 """
+        while True:
+            count += 1
+            match input('\033[32mIn [%d]:\033[0m ' % count):
+                case 'quit' | 'exit' | 'shutdown':
+                    exit()
+                case _:
+                    print('\033[35mOut[%d]:\033[0m Unknow command' % count)
+
+
+class User:
+    """ 用户 """
+
+    def __init__(self) -> None:
+        pass
 
 
 class Client:
@@ -40,14 +75,14 @@ class Client:
     def verify(self) -> bool:
         """ 判断 """
         try:
-            return self.recv()['code'] == 'CLIENT'
+            return self.recv()['op'] == 'CLIENT'
         except:
             return False
 
     def close(self) -> None:
         """ 断开 """
         self.client.close()
-        print('LOST: %s' % self.address)
+        output(INFO, 'LOST: %s' % self.address)
 
     def send(self, **kw) -> None:
         """ 发送 """
@@ -55,18 +90,28 @@ class Client:
 
     def recv(self) -> dict:
         """ 接受 """
-        return eval(self.client.recv(4096).decode())
+        return eval(self.client.recv(PACK_SIZE).decode())
 
     def run(self) -> None:
         """ 执行 """
         if not self.verify():
             return self.client.close()
-        print('CONNECT: %s' % self.address)
+        output(INFO, 'CONNECT: %s' % self.address)
         while True:
             data = self.recv()
-            if data['code'] == 'QUIT':
+            if data['op'] == 'QUIT':
                 return self.close()
+            if data['op'] == 'CODE':
+                code = mail.code()
+                try:
+                    output(INFO, '发送验证码到 %s' % data['mail'])
+                    mail.send(data['mail'], code)
+                except:
+                    output(WARN, '无法发送验证码到 %s！' % data['mail'])
 
 
 if __name__ == '__main__':
-    Server.accept()
+    output(INFO, '启动客户端监听功能')
+    threading.Thread(target=Server.accept, daemon=True).start()
+    output(INFO, '启动服务端命令功能')
+    Server.input()
